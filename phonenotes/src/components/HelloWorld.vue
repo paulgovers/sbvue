b34a44b1600ee77b05295b7317b4076186784afd
Paul Govers <info@paulgovers.com>
2021-10-04 22:14:47 +0200

    initial project setup

---
+++ phonenotes/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+  <div class="col-lg-6">bla</div><div class="col-lg-6">bli</div>
+  <div class="col-xl-1-12">
+    bla
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class HelloWorld extends Vue {
+  msg!: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
